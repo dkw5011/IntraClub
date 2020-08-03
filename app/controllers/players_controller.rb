ec00e49ceeae9de327439ee7b4b2d5977db452c7
dkw5011 <dkw5011@gmail.com>
2020-08-02 23:43:23 -0400

    added form to new view file and updated create method along with adding strong params

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,9 +1,25 @@
 class PlayersController < ApplicationController
    def new
+    @player = Player.new
    end
    
    def create
+    @player = Player.new(player_params)
+        if @player.save
+            session[:player_id] = @player.id
+            redirect_to @player
+        else
+            render :new
+        end
    end
 
-   
+   def show
+   end
+
+   private
+
+   def player_params
+    params.require(:player).permit(:name, :email, :username, :password, :age)
+   end
+
 end
