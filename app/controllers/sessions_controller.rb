87dd8c63a6ca5c8229257cb223c5eadf81cffe98
dkw5011 <dkw5011@gmail.com>
2020-08-09 22:42:52 -0400

    sessions create method build

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,18 @@
 class SessionsController < ApplicationController
-    def home
-    end
 
     def destroy
         session.clear
         redirect_to root_path
+    end
+
+    def create
+        player = Player.find_by(username: params[:player][:username])
+        if user && user.authenticate(params[:player][:username])
+            session[:player_id] = player.id
+            redirect_to player_path(player)
+        else
+            flash[:message] = "Incorrect Login Information Please Try Again"
+            redirect_to "/login"
+        end
     end
 end
